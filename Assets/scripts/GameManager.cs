2305eb0fe87c6778b590e430edfba6c5527a158a
Shenyou <2585324499@qq.com>
2024-10-05 12:32:44 +0800

    传了一些素材
    一些素材和基本的框架

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject spider;
+    public GameObject human;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void run() {
+        
+    }
+}
